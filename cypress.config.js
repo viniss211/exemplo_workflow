a11d9d25ebd3129acafa89ab760bd3b32b79c19b
vinis <viniss211@gmail.com>
2025-07-24 12:30:19 -0300

    adicionando geração de relatório e envio por email ao workflow

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require('cypress');
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      require('cypress-mochawesome-reporter/plugin')(on); // 🔥 ESSENCIAL
+      require('cypress-mochawesome-reporter/plugin')(on); 
     },
     reporter: 'cypress-mochawesome-reporter',
     reporterOptions: {
